9bc0bd4bfdd9048369749105e4cf4b2dd2cb097f
cx <cx0120@163.com>
2022-05-10 22:43:17 +0800

    version-03

--- src/com/cx/git/GitDemo01.java
+++ src/com/cx/git/GitDemo01.java
@@ -4,5 +4,6 @@ public class GitDemo01 {
     public static void main(String[] args) {
         System.out.println("Hello word!!!");
         System.out.println("Hello Git!!!");
+        System.out.println("Hello GitHub!!!");
     }
 }
